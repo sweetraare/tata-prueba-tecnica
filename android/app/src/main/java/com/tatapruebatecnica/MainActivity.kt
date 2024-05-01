7679fed6c9d35a89a626e38db0b6cd54dbd0c56a
Alex <ajua_1696@hotmail.com>
2024-05-01 15:10:21 -0500

    Add react navigation

--- android/app/src/main/java/com/tatapruebatecnica/MainActivity.kt
+++ android/app/src/main/java/com/tatapruebatecnica/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.tatapruebatecnica
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -19,4 +20,9 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+   }
+
 }
