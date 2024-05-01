86f9f88556551c86ace404c914c4070dd3ce43f5
Alex <ajua_1696@hotmail.com>
2024-05-01 17:13:13 -0500

    Fix errors with react-native-svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import { faMoneyBills } from "@fortawesome/free-solid-svg-icons/faMoneyBills";
+
 function Header(): React.JSX.Element {
   return <SafeAreaView>
     <View style={styles.container}>
+      <FontAwesomeIcon icon={faMoneyBills} color="midnightblue" />
       <Text style={styles.text}>BANCO</Text>
     </View>
   </SafeAreaView>;
@@ -14,12 +19,15 @@ const styles = StyleSheet.create(
       backgroundColor: "white",
       paddingVertical: 10,
       display: "flex",
+      flexDirection: "row",
       alignItems: "center",
+      justifyContent: "center",
       borderBottomWidth: 1,
       borderColor: "lightgrey",
     },
     text: {
       color: "midnightblue",
+      paddingHorizontal: 5,
     },
   },
 );
